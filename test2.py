1e2e70db4396ab95cb01b2a8a78e45b1ebf2c985
Ziva1011 <catarina.vi.pires@gmail.com>
2023-05-11 11:19:01 +0200

    training loop

--- test2.py
+++ test2.py
@@ -23,8 +23,11 @@ def main(config: Config):
     train_ds, val_ds, test_ds = NiftiSegCreator.make_datasets(config, (None, None, None))
     train_dl, val_dl, test_dl = NiftiSegCreator.make_dataloader(train_ds, val_ds, test_ds, {}, {},{})
     #train_dl_torch = torch.from_numpy(train_dl)
+
     x, y = next(iter(train_dl))
-    print(x.dtype)
+    x = torch.squeeze(x)
+    x = torch.unsqueeze(x, 0)
+    print(x.shape)
    
     
     model= UNet(in_channels=1,
@@ -39,43 +42,82 @@ def main(config: Config):
     # Testing model
     # x = torch.randn(size=(1, 1, 512, 512, 512), dtype=torch.float32)
     # with torch.no_grad():
-    #     out = model()
+    #     out = model(x)
     # print(f'Out: {out.shape}')
 
     # Option 1: criterion for loss and optimization
     # lossFunc = BCEWithLogitsLoss()
-    # opt = Adam(model.parameters(), lr=1e-3)
+    # opt = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    
+    print(model.parameters)
     #device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     #inputs, labels = inputs.to(device), labels.to(device)
     device = torch.device("cpu")
+
     # criterion
     criterion = torch.nn.CrossEntropyLoss()
 
     # optimizer
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    #optimirzer = torch.optim.Adam(model.parameters(), lr=0.01)
+    
+    trainSteps = len(train_ds)
 
+    print("[INFO] training the network...")
+    for e in range(1):
+        # set the model in training mode
+        model.train()
+        # initialize the total training and validation loss
+        totalTrainLoss = 0
+        totalTestLoss = 0
+        # loop over the training set
+        for (i, (x, y)) in enumerate(train_dl):
+            # send the input to the device
+            (x, y) = (x.to(device), y.to(device))
+            x = x.float()
+            y = y.float()
+            # perform a forward pass and calculate the training loss
+            pred = model(x)
+            pred= torch.argmax(pred, dim=1, keepdim=True)
+
+
+
+            loss = criterion(pred, y)
+            # first, zero out any previously accumulated gradients, then
+            # perform backpropagation, and then update model parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # add the loss to the total training loss so far
+            totalTrainLoss += loss
+        # switch off autograd
+        
+        avgTrainLoss = totalTrainLoss / trainSteps
+        # update our training history
+        # print the model training and validation information
+        print("[INFO] EPOCH: {}/{}".format(e + 1, 1))
+        print("Train loss: {:.6f}".format(
+            avgTrainLoss))
 
 
     # trainer
-    trainer = Trainer(
-        model=model,
-        device=device,
-        criterion=criterion,
-        optimizer=optimizer,
-        training_DataLoader=train_dl,
-        validation_DataLoader=val_dl,
-        lr_scheduler=None,
-        epochs=1,
-        epoch=0,
-        notebook=False,
-    )
+    # trainer = Trainer(
+    #     model=model,
+    #     device=device,
+    #     criterion=criterion,
+    #     optimizer=optimizer,
+    #     training_DataLoader=train_dl,
+    #     validation_DataLoader=val_dl,
+    #     lr_scheduler=None,
+    #     epochs=1,
+    #     epoch=0,
+    #     notebook=False,
+    # )
 
 
     # start training
-    training_losses, validation_losses, lr_rates = trainer.run_trainer()
-    print(training_losses, validation_losses)
+    # training_losses, validation_losses, lr_rates = trainer.run_trainer()
+    # print(training_losses, validation_losses)
 
 if __name__ == '__main__':
     main()
