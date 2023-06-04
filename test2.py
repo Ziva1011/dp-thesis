b404e1e2cb5d981e00e96f1f0ec40d64d256b5f6
Ziva1011 <catarina.vi.pires@gmail.com>
2023-06-04 16:39:32 +0200

    N Adam

--- test2.py
+++ test2.py
@@ -26,18 +26,18 @@ print(Path.cwd())
 def main(config: Config):
     #print(config)
 
-    # wandb.init(
-    #     # set the wandb project where this run will be logged
-    #     project="dpSegmentation",
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="dpSegmentation",
         
-    #     # track hyperparameters and run metadata
-    #     config={
-    #     "learning_rate": 0.01,
-    #     "architecture": "Unet",
-    #     "epochs": 100,
-    #     "loss": "Dice",
-    #     }
-    # )
+        # track hyperparameters and run metadata
+        config={
+        "learning_rate": 0.01,
+        "architecture": "Unet",
+        "epochs": 100,
+        "loss": "Dice",
+        }
+    )
     train_ds, val_ds, test_ds = NiftiSegCreator.make_datasets(
         config, (None, None, None)
     )
@@ -86,81 +86,85 @@ def main(config: Config):
     criterion = DiceLoss(mode, classes=None, log_loss=False, from_logits=True, smooth=0.0, ignore_index=None, eps=1e-07)
 
     # optimizer
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    #optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+    optimizer = torch.optim.NAdam(model.parameters(), lr=0.002)
 
     #f1 = F1Score(task="multiclass", num_classes=3)
 
     trainSteps = len(train_ds)
     epochs= 100
-    # print("[INFO] training the network...")
-    for e in trange(epochs):
-        # set the model in training mode
-        model.train()
-        # initialize the total training and validation loss
-        totalTrainLoss = 0
-        totalTestLoss = 0
-        f1=[0,0,0]
-        # loop over the training set
-        for i, (x, y) in tqdm(enumerate(train_dl), total=len(train_dl), leave=False):
-        # send the input to the device
-            (x, y) = (x.to(device), y.to(device))
-            x = x.float()
-            y = y.squeeze(1).long()
-            #y = y.long()
-            # perform a forward pass and calculate the training loss
-            pred = model(x)
-            # pred = torch.argmax(pred, dim=1, keepdim=True)
-            z= torch.nn.functional.one_hot(y[0], num_classes=3)
-            z = z.view(1,3,128,128,50)
-            loss = criterion(pred, y)
 
-            #tp, fp, fn, tn = smp.metrics.get_stats(z, pred.round().long(), mode='multilabel', threshold=0.5, num_classes=3)
-            #f1_score = smp.metrics.f1_score(tp, fp, fn, tn, reduction="macro-imagewise")
+    # # print("[INFO] training the network...")
+    # for e in trange(epochs):
+    #     # set the model in training mode
+    #     model.train()
+    #     # initialize the total training and validation loss
+    #     totalTrainLoss = 0
+    #     totalTestLoss = 0
+    #     f1=[0,0,0]
+    #     # loop over the training set
+    #     for i, (x, y) in tqdm(enumerate(train_dl), total=len(train_dl), leave=False):
+    #     # send the input to the device
+    #         (x, y) = (x.to(device), y.to(device))
+    #         x = x.float()
+    #         y = y.squeeze(1).long()
+    #         #y = y.long()
+    #         # perform a forward pass and calculate the training loss
+    #         pred = model(x)
+    #         # pred = torch.argmax(pred, dim=1, keepdim=True)
+    #         z= torch.nn.functional.one_hot(y[0], num_classes=3)
+    #         z = z.view(1,3,128,128,50)
+    #         loss = criterion(pred, y)
+
+    #         #tp, fp, fn, tn = smp.metrics.get_stats(z, pred.round().long(), mode='multilabel', threshold=0.5, num_classes=3)
+    #         #f1_score = smp.metrics.f1_score(tp, fp, fn, tn, reduction="macro-imagewise")
             
-            mcf1s = MulticlassF1Score(num_classes=3, average=None).to(device)
-            f1= (f1+mcf1s(pred, y).cpu().numpy())
+    #         mcf1s = MulticlassF1Score(num_classes=3, average=None).to(device)
+    #         f1= (f1+mcf1s(pred, y).cpu().numpy())
 
-            #f1.append(mcf1s(pred, y))
-            #wandb.log({"f1_score": f1.data.item()})
-            #wandb.log({"f1_score": f1_score})
+    #         #f1.append(mcf1s(pred, y))
+    #         #wandb.log({"f1_score": f1.data.item()})
+    #         #wandb.log({"f1_score": f1_score})
 
-            # first, zero out any previously accumulated gradients, then
-            # perform backpropagation, and then update model parameters
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            # add the loss to the total training loss so far
-            totalTrainLoss += loss
-    #         # switch off autograd
+    #         # first, zero out any previously accumulated gradients, then
+    #         # perform backpropagation, and then update model parameters
+    #         optimizer.zero_grad()
+    #         loss.backward()
+    #         optimizer.step()
+    #         # add the loss to the total training loss so far
+    #         totalTrainLoss += loss
+    # #         # switch off autograd
 
-        avgTrainLoss = totalTrainLoss / trainSteps
-        # update our training history
-        f1 = f1/len(train_dl)
-        print(f1)
-        # print the model training and validation information
-        print("[INFO] EPOCH: {}/{}".format(e + 1, 1))
-        #wandb.log({"f1": f1[0]})
-        #wandb.log({"train_loss": avgTrainLoss})
-        #print("F1 score: {:.6f}".format(f1[0]))
-        print("Train loss: {:.6f}".format(avgTrainLoss))
+    #     avgTrainLoss = totalTrainLoss / trainSteps
+    #     # update our training history
+    #     f1 = f1/len(train_dl)
+    #     print(f1)
+    #     # print the model training and validation information
+    #     print("[INFO] EPOCH: {}/{}".format(e + 1, 100))
 
-    # trainer = Trainer(
-    #     model=model,
-    #     device=device,
-    #     criterion=criterion,
-    #     optimizer=optimizer,
-    #     training_DataLoader=train_dl,
-    #     validation_DataLoader=val_dl,
-    #     lr_scheduler=None,
-    #     epochs=100,
-    #     epoch=0,
-    #     notebook=False,
-    # )
+    #     wandb.log({"f1_background": f1[0]})
+    #     wandb.log({"f1_liver": f1[1]})
+    #     wandb.log({"f1_tumor": f1[2]})
+    #     wandb.log({"train_loss": avgTrainLoss})
 
-    # #start training
-    # training_losses, validation_losses, lr_rates = trainer.run_trainer()
-    # #print(training_losses, validation_losses)
+    #     print("Train loss: {:.6f}".format(avgTrainLoss))
+
+    trainer = Trainer(
+        model=model,
+        device=device,
+        criterion=criterion,
+        optimizer=optimizer,
+        training_DataLoader=train_dl,
+        validation_DataLoader=val_dl,
+        lr_scheduler=None,
+        epochs=100,
+        epoch=0,
+        notebook=False,
+    )
+
+    #start training
+    training_losses, validation_losses, lr_rates = trainer.run_trainer()
+    print(training_losses, validation_losses)
 
 
 if __name__ == "__main__":
