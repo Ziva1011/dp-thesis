ec05d312899ba5fe4db17144288f07d21354864a
Ziva1011 <catarina.vi.pires@gmail.com>
2024-03-18 11:11:45 +0100

    psp net 3d

---
+++ segmentation_models/psp/decoder.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from segmentation_models_pytorch.base import modules
+
+try:
+    from inplace_abn import InPlaceABN
+except ImportError:
+    InPlaceABN = None
+
+class Conv3dReLU(nn.Sequential):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        padding=0,
+        stride=1,
+        use_batchnorm=True,
+    ):
+
+        if use_batchnorm == "inplace" and InPlaceABN is None:
+            raise RuntimeError(
+                "In order to use `use_batchnorm='inplace'` inplace_abn package must be installed. "
+                + "To install see: https://github.com/mapillary/inplace_abn"
+            )
+
+        conv = nn.Conv3d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=False,
+        )
+        relu = nn.ReLU(inplace=True)
+
+        if use_batchnorm == "inplace":
+            bn = InPlaceABN(out_channels, activation="leaky_relu", activation_param=0.0)
+            relu = nn.Identity()
+
+        elif use_batchnorm and use_batchnorm != "inplace":
+            bn = nn.BatchNorm3d(out_channels)
+
+        else:
+            bn = nn.Identity()
+
+        super(Conv3dReLU, self).__init__(conv, bn, relu)
+
+
+
+class PSPBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, pool_size, use_bathcnorm=True):
+        super().__init__()
+        if pool_size == 1:
+            use_bathcnorm = False  # PyTorch does not support BatchNorm for 1x1 shape
+        self.pool = nn.Sequential(
+            nn.AdaptiveAvgPool3d(output_size=(pool_size, pool_size, pool_size)),
+            Conv3dReLU(in_channels, out_channels, (1, 1, 1), use_batchnorm=use_bathcnorm),
+        )
+
+    def forward(self, x):
+        h, w, d = x.size(2), x.size(3), x.size(4)
+        x = self.pool(x)
+        x = F.interpolate(x, size=(h, w, d), mode="trilinear", align_corners=True)
+        return x
+
+
+class PSPModule(nn.Module):
+    def __init__(self, in_channels, sizes=(1, 2, 3, 6), use_bathcnorm=True):
+        super().__init__()
+
+        self.blocks = nn.ModuleList(
+            [
+                PSPBlock(
+                    in_channels,
+                    in_channels // len(sizes),
+                    size,
+                    use_bathcnorm=use_bathcnorm,
+                )
+                for size in sizes
+            ]
+        )
+
+    def forward(self, x):
+        xs = [block(x) for block in self.blocks] + [x]
+        x = torch.cat(xs, dim=1)
+        return x
+
+
+class PSPDecoder(nn.Module):
+    def __init__(
+        self,
+        encoder_channels,
+        use_batchnorm=True,
+        out_channels=512,
+        dropout=0.2,
+    ):
+        super().__init__()
+
+        self.psp = PSPModule(
+            in_channels=encoder_channels[-1],
+            sizes=(1, 2, 3, 6),
+            use_bathcnorm=use_batchnorm,
+        )
+
+        self.conv = Conv3dReLU(
+            in_channels=encoder_channels[-1] * 2,
+            out_channels=out_channels,
+            kernel_size=1,
+            use_batchnorm=use_batchnorm,
+        )
+
+        self.dropout = nn.Dropout3d(p=dropout)
+
+    def forward(self, *features):
+        x = features[-1]
+        x = self.psp(x)
+        x = self.conv(x)
+        x = self.dropout(x)
+
+        return x
